c6b5e7bd89fb2cea542df4e95e4f3669c2d02b0d
Nathan <115118374+Nathan4457@users.noreply.github.com>
2023-04-11 15:11:56 +0200

    Add files via upload

--- evaluation_securite/connecte.php
+++ evaluation_securite/connecte.php
@@ -1,36 +1,14 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="inscription.css">
-    <title>Connexion</title>
+    <title>Document</title>
 </head>
 <body>
-<form action="connecte.php" method="post">
 <?php
-include("header.php");
-?>
-
-
-    <div class="container1">
-        <h1>Connexion</h1>
-        <div class="container2">
-            <label id="lab_email" for="email">Adresse mail</label>
-            <input placeholder="Adresse mail" id="email" type="text" name="email">
-
-            <div class="mdp">
-                <label id="lab_mdp" for="mdp">Mot de passe</label>
-                    <input type="password" placeholder="Mot de passe" id="mdp" name="mdp">
-            </div>
-            <input type="submit" name="envoyer">
-        </div>
-    </div>
-
- <?php
     if(isset($_POST['envoyer'])) {
-        die(strlen(password_hash($_POST['mdp'])));
     $dbname = "role_user";
     $dbuser = "root";
     $dbip = "localhost";
@@ -51,6 +29,6 @@ include("header.php");
     }
     }
 ?>
-</form>
+
 </body>
 </html>
