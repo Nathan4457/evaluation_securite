fe44c1cbd74fac54552e2aeeb2b929bb36c9a125
Nathan <115118374+Nathan4457@users.noreply.github.com>
2023-04-12 09:19:10 +0200

    Delete configconnexion.php

--- configconnexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbname = "role_user";
-$dbuser = "root";
-$dbip = "localhost";
-$dbpass = "";
-
-$bdd = new PDO("mysql:host=" . $dbip . ";dbname=" . $dbname . ";charset=utf8", $dbuser, $dbpass);
-?>
